219e7b3ddd7ba181d59c2e16aa061fb6fc1bd590
breezybrow <browder.bree@gmail.com>
2021-06-24 19:09:11 +0000

    Commit history refactor

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Importing, creating variables, dealing with wildcards """
 from flask import Blueprint
-from api.v1.views.index import
+
 
 app_views = Blueprint('app_views', __name__)
+
+from api.v1.views.index import *
