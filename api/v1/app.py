844c6a7fca6db49cd46a9d2b9273962cddb9f21a
breezybrow <browder.bree@gmail.com>
2021-06-28 23:26:25 +0000

    Last one

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,10 @@ from flask import Flask, escape, request, render_template, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
